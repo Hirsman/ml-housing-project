67847cf45f3c06e03357b65f84b5ac32ad0a231f
Hirsman <hirsman.vignolles@gmail.com>
2026-05-12 14:14:34 +0200

    fix ab traffic

--- backend/app.py
+++ backend/app.py
@@ -22,8 +22,10 @@ def predict(data: dict):
 
     start_time = time.time()
 
+    # user id
     user_id = data.pop("user_id", "anonymous")
 
+    # mapping frontend -> modèle
     rename_map = {
         "average_rooms": "AveRooms",
         "average_bedrooms": "AveBedrms",
@@ -35,8 +37,16 @@ def predict(data: dict):
         "longitude": "Longitude",
     }
 
-    data = {rename_map.get(k, k): v for k, v in data.items()}
+    # normalisation
+    normalized_data = {}
 
+    for k, v in data.items():
+        normalized_key = rename_map.get(k, k)
+        normalized_data[normalized_key] = v
+
+    data = normalized_data
+
+    # features attendues par sklearn
     expected_features = [
         "MedInc",
         "HouseAge",
@@ -48,17 +58,23 @@ def predict(data: dict):
         "Longitude",
     ]
 
+    # garantir ordre + colonnes
     data = {k: data.get(k, 0) for k in expected_features}
 
+    # dataframe
     df = pd.DataFrame([data])
 
+    # A/B routing
     variant = choose_variant(user_id)
     model = models[variant]
 
+    # prediction
     prediction = float(model.predict(df)[0])
 
+    # latence
     latency_ms = (time.time() - start_time) * 1000
 
+    # logging expérimentation
     log_prediction(
         {
             "user_id": user_id,
@@ -69,4 +85,9 @@ def predict(data: dict):
         }
     )
 
-    return {"prediction": prediction, "variant": variant, "latency_ms": latency_ms}
+    # réponse API
+    return {
+        "prediction": prediction,
+        "variant": variant,
+        "latency_ms": latency_ms,
+    }
